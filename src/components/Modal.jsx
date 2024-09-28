89c07463ea40ff1277087a43f354b1c09dd7d159
Jash <shahjash2809@gmail.com>
2024-09-28 15:55:51 +0530

    modal changes

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -21,7 +21,10 @@ const Modal = ({ isOpen, onClose, children }) => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-      <div ref={modalRef} className="bg-white p-8 rounded-lg shadow-lg w-full max-w-lg">
+      <div
+        ref={modalRef}
+        className="bg-white p-8 rounded-lg shadow-lg w-full max-w-lg mx-6 sm:mx-0" // Added mx-4 for mobile spacing
+      >
         <button onClick={onClose} className="text-black text-xl absolute top-4 right-4">
           &times;
         </button>
